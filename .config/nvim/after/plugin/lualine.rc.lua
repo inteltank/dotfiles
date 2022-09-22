b0a16afc4d38072c1093a70d68055ec3466d1cfe
Tomoki Sawada <sawadats0127@gmail.com>
2022-09-22 16:16:12 +0900

    feat: replace vim settings to lua

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -1,43 +1,42 @@
-local status, lualine = pcall(require, "lualine")
+local status, lualine = pcall(require, 'lualine')
 if (not status) then return end
 
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'solarized_dark',
-    section_separators = {left = '', right = ''},
-    component_separators = {left = '', right = ''},
+    theme = 'solarized_dark' ,
+    section_separator = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
     disabled_filetypes = {}
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {{
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
-    }},
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { {
+      'filename' ,
+      file_status = true, -- diplays file status
+      path = 0 -- 0 = jsut filename
+    } },
     lualine_x = {
-      { 'diagnostics', sources = {"nvim_diagnostic"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
+      { 'diagnostics', sources = { 'nvim_diagnostic' }, symbols = {  error = ' ', warn = ' ', info = ' ',  hint = ' ' } },
       'encoding',
       'filetype'
     },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {{
       'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      file_status = true,
+      path = 1 -- 1 = relative path
     }},
-    lualine_x = {'location'},
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
-  extensions = {'fugitive'}
+  extensions = { 'fugitive' }
 }
-
